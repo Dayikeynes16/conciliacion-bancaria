e275a007a43665a90dbcfd40e4442cbd67f67959
Dayikeynes16 <msansebastianmorales@gmail.com>
2026-02-24 08:15:13 -0600

    docs: add project architecture documentation and cleanup temporary scripts

--- patch_amounts.php
+++
@@ -1,8 +0,0 @@
-$conciliaciones = App\Models\Conciliacion::where('monto_aplicado', 0)->get();
-foreach ($conciliaciones as $c) {
-    if ($c->movimiento && $c->factura) {
-        $c->monto_aplicado = min($c->factura->monto, $c->movimiento->monto);
-        $c->save();
-        echo "Updated conciliacion {$c->id} to {$c->monto_aplicado}\n";
-    }
-}
